08c0176c23af64570a9b67490ff0d1c72139f346
wendevlife <frouenmedinajr@gmail.com>
2025-07-31 14:29:04 +0800

    Added DashboardView.dart

---
+++ lib/src/view/DashboardView.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DashboardView extends StatelessWidget {
+
+  const DashboardView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to the Dashboard',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+
+}
