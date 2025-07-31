1351efb5ed49c096fd43f7af9ba3a6182d1916bc
wendevlife <frouenmedinajr@gmail.com>
2025-07-31 13:56:17 +0800

    Done designing the SplashView.dart and fix the gradle issue

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.disaster.reflief.disaster_relief_coordination"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 23
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
---
+++ lib/src/view/SplashView.dart
@@ -0,0 +1,75 @@
+import 'package:disaster_relief_coordination/src/view/GetStartedView.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../helpers/ColorHelpers.dart';
+import '../helpers/ImageHelper.dart';
+import '../widgets/CustomText.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  _SplashViewState createState() => _SplashViewState();
+
+}
+
+class _SplashViewState extends State<SplashView> {
+
+  bool isLoading = true;
+
+  void initState() {
+    super.initState();
+    // Simulate a delay for loading
+    Future.delayed(Duration(seconds: 3), () {
+      setState(() {
+        isLoading = false;
+      });
+      // Navigate to the next screen or perform any action after loading
+      Navigator.pushReplacement(context,  MaterialPageRoute(builder: (context) {
+        return GetStartedView();
+      }));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: screenWidth * 0.9,
+              height: screenHeight * 0.3,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(ImageHelper.logoPath),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.02),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.1),
+              child:   CustomText(text: 'Disaster Relief Coordination',
+                  fontFamily: 'Anton',
+                  fontSize: 30.0,
+                  color: ColorHelpers.primaryColor,
+                  fontWeight: FontWeight.w400,
+                  textAlign:  TextAlign.center
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.02),
+            isLoading
+                ? CircularProgressIndicator(
+              color: ColorHelpers.primaryColor,
+            )
+                : SizedBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
