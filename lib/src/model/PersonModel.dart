1029cec7ddb736e6264b815f253dbe2f00da1980
wendevlife <frouenmedinajr@gmail.com>
2025-08-04 09:06:51 +0800

    Added PersonModel.dart

---
+++ lib/src/model/PersonModel.dart
@@ -0,0 +1,14 @@
+class PersonModel {
+  final String id;
+   final String name;
+   final String status;
+
+  PersonModel({
+    required this.id,
+    required this.name,
+    required this.status,
+  });
+
+
+
+}
